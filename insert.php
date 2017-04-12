c6b21ce2bda72fb399914cd69c2ea4fde4e99bb2
gportillo <gportillo94@gmail.com>
2017-04-11 20:04:29 -0500

    print conn

--- insert.php
+++ insert.php
@@ -1,32 +1,33 @@
 <?php
-$servername = "us-cdbr-iron-east-03.cleardb.net";
-$username = "bfd1849f40443a";
-$password = "fcae7675";
-$dbname = "ad_7c1f0caae9b13ee";
+	print("select"); 
+	$servername = "us-cdbr-iron-east-03.cleardb.net";
+	$username = "bfd1849f40443a";
+	$password = "fcae7675";
+	$dbname = "ad_7c1f0caae9b13ee";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-	print($conn) ; 
-    die("Connection failed: " . $conn->connect_error);
- }
+	// Check connection
+	if ($conn->connect_error) {
+		print($conn) ; 
+		die("Connection failed: " . $conn->connect_error);
+	}
 
-// prepare and bind
-$stmt = $conn->prepare("INSERT INTO directorio (clave, nombre, direccion, telefono, correo) VALUES (?, ?, ?, ?, ?)");
-$stmt->bind_param("issss", $clave, $nombre, $direccion, $telefono, $correo);
+	// prepare and bind
+	$stmt = $conn->prepare("INSERT INTO directorio (clave, nombre, direccion, telefono, correo) VALUES (?, ?, ?, ?, ?)");
+	$stmt->bind_param("issss", $clave, $nombre, $direccion, $telefono, $correo);
 
-// set parameters and execute
-$clave = $_POST["clave"];
-$nombre = $_POST["nombre"];
-$direccion = $_POST["direccion"];
-$telefono = $_POST["telefono"];
-$correo = $_POST["correo"];
-$stmt->execute();
+	// set parameters and execute
+	$clave = $_POST["clave"];
+	$nombre = $_POST["nombre"];
+	$direccion = $_POST["direccion"];
+	$telefono = $_POST["telefono"];
+	$correo = $_POST["correo"];
+	$stmt->execute();
 
-echo "successfully";
+	echo "successfully";
 
-$stmt->close();
- $conn->close();
+	$stmt->close();
+	$conn->close();
 ?>
